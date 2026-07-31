8947ce43774bd4225cce757e940d02c0e191a6c6
erarunsingh1998-sketch <er.arunsingh1998@gmail.com>
2026-07-31 13:53:09 +0530

    dynamic fixes

--- app/analyseResume/page.js
+++ app/analyseResume/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import AnalyzeComp from "@/components/AnalyzePage/AnalyzeComp";
 import HomeHeader from "@/components/HomePage/Header";
 
@@ -9,6 +10,8 @@ export default function AnalyzePage(){
 
   return <div className="min-h-screen w-full flex flex-col">
     <HomeHeader />
-    <AnalyzeComp />
+    <Suspense fallback={<div className="mx-auto w-full px-4 py-16 text-center text-slate-700">Loading analysis...</div>}>
+      <AnalyzeComp />
+    </Suspense>
   </div>
 }
