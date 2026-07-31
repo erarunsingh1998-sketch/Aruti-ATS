bbe53f8a8e8411a5a2bb0daf87b9b7760d6e28be
erarunsingh1998-sketch <er.arunsingh1998@gmail.com>
2026-07-31 13:36:27 +0530

    dynamic fix

--- app/analyseResume/page.js
+++ app/analyseResume/page.js
@@ -3,6 +3,8 @@
 import AnalyzeComp from "@/components/AnalyzePage/AnalyzeComp";
 import HomeHeader from "@/components/HomePage/Header";
 
+export const dynamic = 'force-dynamic';
+
 export default function AnalyzePage(){
 
   return <div className="min-h-screen w-full flex flex-col">
